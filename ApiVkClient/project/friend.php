10cfc3f7b93dbcd8b9587eb1054f6888c1d61eee
RomanSeliverstov <nokian2h@gmail.com>
2016-01-05 04:24:30 +0500

    Add project

---
+++ ApiVkClient/project/friend.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class friend extends Model
+{
+    //article - это название таблицы в базе данных.
+     protected $table= 'friends';
+}
+?>
